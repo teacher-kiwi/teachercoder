92fc9d8911db94b0ab22dea04cb862af6f19e7e3
soave424 <89090628+soave424@users.noreply.github.com>
2021-09-18 00:11:41 +0900

    Update D1.py

--- soave424/D1.py
+++ soave424/D1.py
@@ -1,8 +1,10 @@
+# 자릿값 합이 k인 n번째 수 구하기 
+# TEST 결과 : Wrong
 kn=input()
 k=int(kn.split()[0])
 n=int(kn.split()[1])
 list_ksum=[]
-for number in range(999):
+for number in range(70000):
   result=0
   for i in str(number):
     result += int(i)
