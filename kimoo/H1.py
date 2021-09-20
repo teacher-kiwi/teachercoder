9b97017a2709dd5934c594c526d3a6981620a78a
teacher-kiwi <kwi07@hanmail.net>
2021-09-21 06:48:36 +0900

    H 문제 해결

---
+++ kimoo/H1.py
@@ -0,0 +1,15 @@
+data = input().split()
+p = int(data[0])
+day = int(data[1])
+
+def v(p, day):
+    if day == 0:
+        return p
+    elif day == 1:
+        return p
+    elif day == 2:
+        return p + (v(p, day-2) * 2)
+    else:
+        return p + (v(p, day-2) * 2) + v(p, day-3)
+    
+print(v(p, day))
