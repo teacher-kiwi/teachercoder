b5075b7ec52e52b0c40c9c9a51e5403c20daee75
soave424 <soave424@gmail.com>
2023-10-16 20:33:10 +0900

    b1

---
+++ soave424/2023/Pre/b0.py
@@ -0,0 +1,11 @@
+a, b, c, d = 0, 1, 4, 9
+
+
+def check_zero(count, a, b, c, d):
+    if (a, b, c, d) == (0, 0, 0, 0):
+        print(count)
+    else:
+        check_zero(count+1, abs(a-b), abs(b-c), abs(c-d), abs(d-a))
+
+
+check_zero(0, a, b, c, d)
