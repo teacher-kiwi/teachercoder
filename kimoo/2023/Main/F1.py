4b44b03a4a648d5c8239453ddc519265a1e2f17d
soave424 <soave424@gmail.com>
2023-10-21 13:14:45 +0900

    문제

--- kimoo/2023/Main/F1.py
+++ kimoo/2023/Main/F1.py
@@ -1,13 +1,13 @@
 n = int(input(), 2)
-
 res = [float("inf")]
-
 stack = [(0, n)]
+
+
 def dfs(i, n):
     if n == 0:
         res[0] = min(i, res[0])
         return
-    
+
     mask = 1 << (len(bin(n)) - 3)
     dfs(i+1, n ^ mask)
 
@@ -15,6 +15,7 @@ def dfs(i, n):
     for j in range(len(bin(n)) - 2):
         mask |= (1 << j)
     dfs(i+1, n ^ mask)
+
 
 b = 1
 while b < n:
