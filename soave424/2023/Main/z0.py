48e29376bc252e0509e64e23c9eb4f35aebe2add
soave424 <soave424@gmail.com>
2023-10-21 10:18:25 +0900

    e

--- soave424/2023/Main/z0.py
+++ soave424/2023/Main/z0.py
@@ -1,11 +1,15 @@
-# 참가자수 (2*21, 3*17)
-people = 93
+# 참가자수
+people = 74
 
 # 참가팀 수
-team = 38
+team = 30
 
 # 공식명칭
 name = "Gyeonggi Teachers Programming Challenge"
+nameC = "GTPC"
 
 # 회차
 time = 5
+
+
+print(nameC)
